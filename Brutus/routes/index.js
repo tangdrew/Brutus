2cd7b5b052895355782efb5d86e26acf618d9017
tangdrew <andrewtang@sbcglobal.net>
2015-08-29 14:21:21 -0500

    Set up the express project, added all of the front end views we built to the correct places in the project, including search page, course listings, and login/register

--- Brutus/routes/index.js
+++ Brutus/routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET search page. */
+router.get('/search', function(req, res, next) {
+  res.render('search', { title: 'Search' });
+});
+
+/* GET course profile page. */
+router.get('/course', function(req, res, next) {
+  res.render('course', { title: 'Course' });
+});
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login to Brutus' });
+});
+
+/* GET registration page. */
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Register for Brutus' });
+});
+
 module.exports = router;
