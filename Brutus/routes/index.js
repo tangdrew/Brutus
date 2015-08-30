2fe20bcc33e7a66fa0d2922660e71be4b5dcb02a
cellist1993 <cellist1993@yahoo.co.kr>
2015-08-29 23:25:47 -0500

    complete login and registration front/back end

--- Brutus/routes/index.js
+++ Brutus/routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var accounts = require('../modules/accounts');
+var cookieParser = require('cookie-parser');
+var session = require('express-session')
+
+router.use(cookieParser());
+router.use(session({secret: '1234567890QWERTY',resave: false,saveUninitialized: true}));
 
 /* GET home page. 
 router.get('/', function(req, res, next) {
@@ -8,66 +13,119 @@ router.get('/', function(req, res, next) {
 });*/
 
 /* GET search page. */
-router.get('/search', function(req, res, next) {
-  res.render('search', { title: 'Search' });
+router.get('/search', function(req, res, next) 
+{
+    res.render('search', { user: req.session.user, title: 'Search' });
 });
 
 /* GET course profile page. */
-router.get('/course', function(req, res, next) {
-  res.render('course', { title: 'Course' });
+router.get('/course', function(req, res, next) 
+{
+  if(req.session.user == undefined)
+  {
+      res.render('login', { title: 'Login to Brutus' });   
+  }
+  else
+  {
+      res.render('course', { user: req.session.user, title: 'Course' });  
+  }  
 });
 
 /* GET login page. */
-router.get('/login', function(req, res, next) {
-  res.render('login', { title: 'Login to Brutus' });
+router.get('/login', function(req, res, next) 
+{
+  // Check if user is already logged in. If they are, redirect them to their account page
+  if (req.session.user == undefined)
+  {
+      res.render('login', { title: 'Login to Brutus' }); 
+  }
+  else
+  {
+      res.redirect('accountSummary');
+  }  
 });
 
-router.post('/login', function(req, res, next) {
+router.post('/login', function(req, res, next) 
+{
    accounts.checkLogin({
        email : req.body['email'],
        password : req.body['password']
-   }, function(e){
-       if(e != 'success'){
-           res.render('login', {error : e});
-       }
-       else{
+   }, 
+   function(e, o){       
+       if(e == 'fail')
+       {
+           res.render('login', { error : e });
+       }       
+       else
+       {
+           req.session.user = o;
+           if (req.body['remember-me'] == 'true')
+           {
+					   res.cookie('user', o.email, { maxAge: 900000 });
+					   res.cookie('pass', o.password, { maxAge: 900000 });
+				   }
            res.redirect('search');
        }
    }); 
 });
 
 /* GET registration page. */
-router.get('/register', function(req, res, next) {
+router.get('/register', function(req, res, next) 
+{
   res.render('register', { title: 'Register for Brutus' });
 });
 
-router.post('/register', function(req, res){
+router.post('/register', function(req, res)
+{
     accounts.createAccount({
-			fistName 	: req.body['firstName'],
+			      firstName 	: req.body['firstName'],
             lastName 	: req.body['lastName'],
             email 	: req.body['email'],
             password	: req.body['password'],
             passwordConfirm : req.body['passwordConfirm']
-		}, function(e){
-			if (e){
-				//res.status(400).send(e);
-                res.render('register', { error: e });
-			}	else{
+	}, 
+  function(e)
+  {
+			if (e == 'email-taken')
+      {				
+                res.render('register', { emailTaken : e });
+			}
+      else if (e == 'missingData')
+      {				
+                res.render('register', { missingData : e });
+			}
+      else if (e == 'invalidEmail')
+      {
+                res.render('register', { invalidEmail : e });
+      }
+      else if (e == 'passwordNotMatch')
+      {				
+                res.render('register', { passwordNotMatch : e });
+			}	
+      else
+      {
 				res.render('login', { registered: true });
 			}
-		});
+	});
 });
 
-/*GET index page, if cookie saved proceed to home, else go to login */
-router.get('/', function(req, res){
-// check if the user's credentials are saved in a cookie //
-   if (req.cookies.user == undefined || req.cookies.pass == undefined){
-      res.render('login', 
-         { locals: 
-            { title: 'Hello - Please Login To Your Account' }
-         }
-      );
-   }
+// GET logout page
+router.get('/logout', function(req, res, next)
+{
+    res.render('index');
+});
+
+router.post('/logout', function(req, res)
+{
+    res.clearCookie('user');
+		res.clearCookie('pass');
+		req.session.destroy();
+});
+
+/*GET index page, if cookie saved proceed to home, else go to login*/ 
+router.get('/', function(req, res)
+{
+      res.redirect('index');
 });
 
 module.exports = router;
