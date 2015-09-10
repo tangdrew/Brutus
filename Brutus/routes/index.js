aeca2588bfb3572d1c230483568eda0233a4e8e4
cellist1993 <cellist1993@yahoo.co.kr>
2015-09-09 19:17:24 -0500

    changed email address

--- Brutus/routes/index.js
+++ Brutus/routes/index.js
@@ -274,7 +274,7 @@ router.post('/feedback', function(req, res)
 {
     var sendgrid = require('sendgrid')('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');
     sendgrid.send({
-        to      : 'yongpark2016@u.northwestern.edu',
+        to      : 'nubrutus@gmail.com',
         from    : 'feedback@brutus.com',
         subject : 'User Feedback from: ' + req.session.user.firstName + ' ' + req.session.user.lastName,
         text    :  req.body['feedbacks']   
